c45c74336db3468b5fe8758b920443e10236edee
AntDimi <94530448+AntDimi@users.noreply.github.com>
2022-04-18 18:54:14 +0100

    Add files via upload

--- src/main/java/com/lhm/secondhandstore/service/AuthenticationService.java
+++ src/main/java/com/lhm/secondhandstore/service/AuthenticationService.java
@@ -1,1 +1,25 @@
+package com.lhm.secondhandstore.service;
 
+import java.util.Base64.Encoder;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lhm.secondhandstore.model.StudentEntity;
+import com.lhm.secondhandstore.repository.StudentRepository;
+
+@Service
+public class AuthenticationService {
+	@Autowired
+	private StudentRepository studentRepository;
+
+	public boolean isValidUser(String email, String passWord) {
+		StudentEntity studententity=studentRepository.findByEmail(email);
+		if(null!=studententity) {
+			return true;
+		}
+		return false;
+	}
+
+}
