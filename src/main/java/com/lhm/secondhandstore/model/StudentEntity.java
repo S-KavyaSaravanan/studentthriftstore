4f37809155259a81761023ffca88ed6571c3b327
AntDimi <94530448+AntDimi@users.noreply.github.com>
2022-04-18 18:55:47 +0100

    Add files via upload

--- src/main/java/com/lhm/secondhandstore/model/StudentEntity.java
+++ src/main/java/com/lhm/secondhandstore/model/StudentEntity.java
@@ -1,1 +1,69 @@
+package com.lhm.secondhandstore.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+	@Entity
+	@Table(name = "student")
+	public class StudentEntity {
+
+	    @Id
+	    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	    private Long id;
+	    private String firstName;
+	    private String lastName;
+	    @Column(unique = true)
+	    private String studentEmail;
+	    private String password;
+	    private String studentAddress;
+	    private String studentPostcode;
+	    public Long getId() {
+			return id;
+		}
+		public void setId(Long id) {
+			this.id = id;
+		}
+		public String getFirstName() {
+			return firstName;
+		}
+		public void setFirstName(String firstName) {
+			this.firstName = firstName;
+		}
+		public String getLastName() {
+			return lastName;
+		}
+		public void setLastName(String lastName) {
+			this.lastName = lastName;
+		}
+		public String getStudentEmail() {
+			return studentEmail;
+		}
+		public void setStudentEmail(String studentEmail) {
+			this.studentEmail = studentEmail;
+		}
+		public String getPassword() {
+			return password;
+		}
+		public void setPassword(String password) {
+			this.password = password;
+		}
+		public String getStudentAddress() {
+			return studentAddress;
+		}
+		public void setStudentAddress(String studentAddress) {
+			this.studentAddress = studentAddress;
+		}
+		public String getStudentPostcode() {
+			return studentPostcode;
+		}
+		public void setStudentPostcode(String studentPostcode) {
+			this.studentPostcode = studentPostcode;
+		}
+
+	    //getter setters
+	}
 
