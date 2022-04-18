daf028e5fbf2395b797d9d6b4936c9b49a466534
AntDimi <94530448+AntDimi@users.noreply.github.com>
2022-04-18 18:55:07 +0100

    Add files via upload

--- src/main/java/com/lhm/secondhandstore/repository/StudentRepository.java
+++ src/main/java/com/lhm/secondhandstore/repository/StudentRepository.java
@@ -1,1 +1,12 @@
+package com.lhm.secondhandstore.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.lhm.secondhandstore.model.StudentEntity;
+
+@Repository
+public interface StudentRepository extends JpaRepository < StudentEntity, Long > {
+	    StudentEntity findByEmail(String studentEmail);
+	}
 
