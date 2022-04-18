3fff445dad37076898137532f097a06e716d69c5
AntDimi <94530448+AntDimi@users.noreply.github.com>
2022-04-18 18:52:45 +0100

    Add files via upload
    login

--- src/main/java/com/lhm/secondhandstore/Api/AutenticationApi.java
+++ src/main/java/com/lhm/secondhandstore/Api/AutenticationApi.java
@@ -1,1 +1,20 @@
+package com.lhm.secondhandstore.Api;
 
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+public interface AutenticationApi {
+@RequestMapping(value = "/login",  consumes="application/json", produces = {
+"application/json" }, method = RequestMethod.POST)
+ResponseEntity<?> login(@RequestBody String loginDetails);
+
+@RequestMapping(value = "/logout",  consumes="application/json", produces = {
+"application/json" }, method = RequestMethod.GET)
+ResponseEntity<?> logOut();
+
+}
